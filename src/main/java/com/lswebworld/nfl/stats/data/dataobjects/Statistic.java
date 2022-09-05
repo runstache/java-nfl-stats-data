9d21f259aa96770e09ab2e6e6b2c99ef1f13e3e3
Larry Shields <larry.shields@gmail.com>
2022-09-05 15:54:32 -0400

    Updated Statistics Table Name.

--- src/main/java/com/lswebworld/nfl/stats/data/dataobjects/Statistic.java
+++ src/main/java/com/lswebworld/nfl/stats/data/dataobjects/Statistic.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -18,6 +19,7 @@ import lombok.Setter;
  */
 
 @Entity
+@Table(name = "statistics")
 @Getter
 @Setter
 @EqualsAndHashCode
